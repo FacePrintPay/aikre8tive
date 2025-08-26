97308d5dc7c438c962b2425d0a24898442780d4f
FacePrintPay <admin@faceprintpay.com>
2025-08-26 19:15:45 -0400

    🛰️ One-Bash Sovereign Sync by #MrGGTP

---
+++ cpp_gateway/build/CMakeFiles/gateway.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gateway.dir/link.d"
+  "CMakeFiles/gateway.dir/main.cpp.o"
+  "CMakeFiles/gateway.dir/main.cpp.o.d"
+  "gateway"
+  "gateway.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gateway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
